5da8b6eab9740907ce39bfdf3804594b2c6d4b6f
ogen <40736025+czg0099@users.noreply.github.com>
2021-02-19 09:59:06 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,20 @@
 #! /bin/sh -e
 df -h
+echo -------------------------
 cat /proc/cpuinfo
+echo -------------------------
 free -h
+echo -------------------------
 ls -l /usr/bin
 ls -l /usr/local/bin
+echo -------------------------
 ps -afx
 echo -------------------------
 #cat /home/travis/build.sh
-CFLAGS="-g" ./build.sh
+#CFLAGS="-g" ./build.sh
 
-if [ ! -d .tup ]; then
-	./build/tup init
-fi
-./build/tup upd
+#if [ ! -d .tup ]; then
+#	./build/tup init
+#fi
+#./build/tup upd
 echo "Build complete. If ./tup works, you can remove the 'build' directory."
